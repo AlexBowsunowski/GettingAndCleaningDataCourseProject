5b8240d2d7bcf53a70540a4ebc3543c3db9e0cf3
AlexBowsunowski <63734939+AlexBowsunowski@users.noreply.github.com>
2021-03-11 11:20:21 +0300

    Add files via upload

---
+++ cleaningDataCourseProject.R
@@ -0,0 +1,49 @@
+setwd("/home/aleksandr/Documents/Rprojects/cleaningData/UCI_HAR_Dataset")
+directory <- "/home/aleksandr/Documents/Rprojects/cleaningData/UCI_HAR_Dataset" 
+pathdata <- file.path(directory)
+#read data
+x_train <- read.table(file.path(pathdata, "train", "X_train.txt"), header = F)
+y_train <- read.table(file.path(pathdata, "train", "y_train.txt"),header = F)
+subject_train <- read.table(file.path(pathdata, "train", "subject_train.txt"),header = F)
+
+x_test = read.table(file.path(pathdata, "test", "X_test.txt"),header = FALSE)
+y_test = read.table(file.path(pathdata, "test", "y_test.txt"),header = FALSE)
+subject_test = read.table(file.path(pathdata, "test", "subject_test.txt"),header = FALSE)
+
+features = read.table(file.path(pathdata, "features.txt"),header = FALSE)
+
+activityLabels = read.table(file.path(pathdata, "activity_labels.txt"),header = FALSE)
+
+
+#Rename the name of the data column
+colnames(x_train) <- features[,2]
+colnames(x_test) <- features[,2]
+
+colnames(y_train) <- "activityID"
+colnames(subject_train) <- "subjectID"
+
+colnames(y_test) <- "activityID"
+colnames(subject_test) <- "subjectID"
+
+colnames(activityLabels) <- c("activityID", "activityType")
+
+#Merge training data and testing data
+train <- cbind(x_train, subject_train, y_train)
+test <- cbind(x_test, subject_test, y_test)
+data <- rbind(train, test)
+
+cnames <- colnames(data)
+
+#Extracts only the measurements on the mean and standard deviation for each measurement. 
+data_for_mean_std <- (grepl("activityID" , cnames) | grepl("subjectID" , cnames) | grepl("mean.." , cnames) | grepl("std.." , cnames))
+dataMean_Std <- data[, data_for_mean_std == T] 
+
+#Appropriately labels the data set with descriptive variable names. 
+dataActivity <- merge(dataMean_Std, activityLabels, by = 'activityID', all.x = T)
+dataActivity[1,]$activityType
+
+str(dataActivity)
+dataActivity$activityType
+tidySet <- aggregate(. ~subjectID + activityID, dataActivity, mean)
+tidySet <- tidySet[order(tidySet$subjectID, tidySet$activityID),]
+write.table(tidySet, "TidySet.txt", row.name = F)
